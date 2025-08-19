7252459f4d671b06153ca96fa3324847291d9882
Adnan Mirzai <Adnanhmirzai@gmail.com>
2025-08-19 19:15:23 +0200

    U2 done

--- F0/U2/src/Main.java
+++ F0/U2/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Scanner scan = new Scanner(System.in);
+        int n = 5;
+        int[] numbers = new int[n];
+        int buff;
+
+        System.out.println("Please input whole numbers");
+        for(int i=0; i<numbers.length; i++) {
+            buff = scan.nextInt();
+            if(buff>0) {
+                numbers[i] = buff;
+            }
+            else {
+                System.out.println("ERROR: NOT WHOLE NUMBER!");
+                i--;
+            }
+        }
+
+        for(int i=numbers.length-1; i>=0; i--) {
+            System.out.println(numbers[i]);
+        }
     }
 }
