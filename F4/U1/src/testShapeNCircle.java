d112fcccfaf921d295dd8e758cd80ef17631c5b8
Adnan Mirzai <Adnanhmirzai@gmail.com>
2025-09-11 18:51:13 +0200

    down/upcast

--- F4/U1/src/testShapeNCircle.java
+++ F4/U1/src/testShapeNCircle.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+
 public class testShapeNCircle {
     public static void main(String[] args) {
         Shape myShape = new Shape(10, 20);
@@ -12,8 +14,24 @@ public class testShapeNCircle {
         System.out.println(myCircle.getX());
         System.out.println(myCircle.toString());
 
-        Circle c = new Circle(20, 20, 20); //POLYMORPHISM, circle is a shape, therefor we can upcast!
+        Circle c = new Circle(20, 20, 20); //POLYMORPHISM, circle is a shape, therefor we can upcast! special -> general
         Shape cs = c;
         System.out.println(c.toString());
+
+        Rectangle myRectangle = new Rectangle();
+
+
+        Shape[] shapes = new Shape[10];
+        shapes[0] = myCircle;
+        shapes[1] = myShape;
+        shapes[2] = myRectangle;
+
+        System.out.println(shapes[0].toString());
+        System.out.println(shapes[1].toString());
+        System.out.println(shapes[2].toString());
+
+        Circle b = (Circle) shapes[0];
+        System.out.println(b.getRadius()); //DOWNCAST from general -> special
+
     }
 }
