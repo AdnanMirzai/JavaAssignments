fcf0d72705bab9d1304a04dabd1fecf54d4edceb
Adnan Mirzai <Adnanhmirzai@gmail.com>
2025-09-19 21:15:45 +0200

    fixed converting issue

--- F4/U5/money/Money.java
+++ F4/U5/money/Money.java
@@ -1,44 +1,52 @@
 public class Money implements IMoney2 {
 
-    double amount;
-    Currency currency;
+    private final double amount;
+    private final Currency currency;
 
-    public Money(double amount, Currency currency) {
-        if(amount<0) throw new IllegalArgumentException("Negative amount");
+    public Money(double amount, Currency currency) throws IllegalArgumentException {
+        if (amount < 0) throw new IllegalArgumentException("Negative amount");
         this.amount = amount;
         this.currency = currency;
     }
 
-    public Money(double amount) {
-        this(amount, Currency.EUR);
+    public Money(Currency currency) {
+        this(0, currency);
     }
 
     public Money() {
-       this(0,Currency.EUR);
+        this(0, Currency.EUR);
     }
 
     @Override
-    public double getAmount() { return amount; }
+    public double getAmount() {
+        return amount;
+    }
 
     @Override
-    public Currency getCurrency() { return currency; }
+    public Currency getCurrency() {
+        return currency;
+    }
 
     @Override
-    public Money copy() { return new Money(this.amount, this.currency); }
+    public Money copy() {
+        return new Money(this.amount, this.currency);
+    }
 
     @Override
-    public Money toCurrency(Currency newCurrency) { return new Money(this.amount, newCurrency); }
+    public Money toCurrency(Currency newCurrency) {
+        return new Money(calculateAmountIn(newCurrency), newCurrency);
+    }
 
     @Override
     public Money add(Money moneyToAdd) {
-        double sum = moneyToAdd.amount + this.amount;
+        double sum = moneyToAdd.calculateAmountIn(this.currency) + this.amount;
         return new Money(sum, this.currency);
     }
 
     @Override
     public Money subtract(Money moneyToAdd) throws IllegalArgumentException {
         double sum = moneyToAdd.amount + this.amount;
-        if(sum < 0) throw new IllegalArgumentException();
+        if (sum < 0) throw new IllegalArgumentException();
         return new Money(sum, this.currency);
     }
 
@@ -51,22 +59,22 @@ public class Money implements IMoney2 {
     public int compareTo(Money other) {
         double thisInEuro = this.calculateAmountIn(Currency.EUR);
         double otherInEuro = other.calculateAmountIn(Currency.EUR);
-        if(Math.abs(thisInEuro-otherInEuro)<1E-3) {
+        if (Math.abs(thisInEuro - otherInEuro) < 1E-3) {
             return 0;
-        }
-        else if (thisInEuro>otherInEuro) {
+        } else if (thisInEuro > otherInEuro) {
             return 1;
-        }
-        else return -1;
+        } else return -1;
     }
 
     @Override
     public boolean equals(Object other) {
-        return this.compareTo((Money) other) == 0;
+        if (other instanceof Money) {
+            return this.compareTo((Money) other) == 0;
+        }
+        return false;
     }
 
     private double calculateAmountIn(Currency newCurrency) {
-        return amount * this.currency.getRate()/newCurrency.getRate();
+        return amount * this.currency.getRate() / newCurrency.getRate();
     }
-
 }
--- F4/U5/money/testMoney.java
+++ F4/U5/money/testMoney.java
@@ -1,7 +1,7 @@
 public class testMoney {
     public static void main(String[] args) {
         Money mymoney = new Money(5000, Currency.SEK);
-        Money test = new Money(5000);
+        Money test = new Money(Currency.NOK);
         System.out.println(mymoney.toString());
         System.out.println(test.toString());
 
