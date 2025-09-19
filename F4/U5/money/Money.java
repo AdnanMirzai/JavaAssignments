4a62c147bf9ea00cfa90d02e3d64920f50492383
Adnan Mirzai <Adnanhmirzai@gmail.com>
2025-09-19 19:27:23 +0200

    Learned to downcast

--- F4/U5/money/Money.java
+++ F4/U5/money/Money.java
@@ -1,37 +1,41 @@
-
 public class Money implements IMoney2 {
 
     double amount;
     Currency currency;
 
     public Money(double amount, Currency currency) {
+        if(amount<0) throw new IllegalArgumentException("Negative amount");
         this.amount = amount;
         this.currency = currency;
     }
 
     public Money(double amount) {
-        this.amount = amount;
-        this.currency = Currency.EUR;
+        this(amount, Currency.EUR);
     }
 
     public Money() {
-        this.amount = 0;
-        this.currency = Currency.EUR;
+       this(0,Currency.EUR);
     }
 
+    @Override
     public double getAmount() { return amount; }
 
+    @Override
     public Currency getCurrency() { return currency; }
 
+    @Override
     public Money copy() { return new Money(this.amount, this.currency); }
 
+    @Override
     public Money toCurrency(Currency newCurrency) { return new Money(this.amount, newCurrency); }
 
+    @Override
     public Money add(Money moneyToAdd) {
         double sum = moneyToAdd.amount + this.amount;
         return new Money(sum, this.currency);
     }
 
+    @Override
     public Money subtract(Money moneyToAdd) throws IllegalArgumentException {
         double sum = moneyToAdd.amount + this.amount;
         if(sum < 0) throw new IllegalArgumentException();
@@ -42,10 +46,27 @@ public class Money implements IMoney2 {
     public String toString() {
         return String.format("Amount %.2f, Currency: %s", amount, currency);
     }
+
     @Override
     public int compareTo(Money other) {
-        return 0;
+        double thisInEuro = this.calculateAmountIn(Currency.EUR);
+        double otherInEuro = other.calculateAmountIn(Currency.EUR);
+        if(Math.abs(thisInEuro-otherInEuro)<1E-3) {
+            return 0;
+        }
+        else if (thisInEuro>otherInEuro) {
+            return 1;
+        }
+        else return -1;
     }
 
+    @Override
+    public boolean equals(Object other) {
+        return this.compareTo((Money) other) == 0;
+    }
+
+    private double calculateAmountIn(Currency newCurrency) {
+        return amount * this.currency.getRate()/newCurrency.getRate();
+    }
 
 }
--- F4/U5/money/testMoney.java
+++ F4/U5/money/testMoney.java
@@ -1,6 +1,11 @@
 public class testMoney {
     public static void main(String[] args) {
         Money mymoney = new Money(5000, Currency.SEK);
+        Money test = new Money(5000);
         System.out.println(mymoney.toString());
+        System.out.println(test.toString());
+
+        System.out.println(mymoney.compareTo(test));
+        System.out.println(mymoney.equals(test));
     }
 }
